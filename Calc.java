9cf46fa580f4ebc24da4f6176b7a0642007938ab
kaulwarv15 <72965622+kaulwarv15@users.noreply.github.com>
2020-10-20 11:18:05 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -8,9 +8,14 @@ public static int sub(int a, int b)
 {
 return a-b;
 }
+public static int prod(int a, int b)
+{
+return a*b;
+}
 public static void main(String[] args)
 {
   System.out.println(add(10,20));
+  System.out.println(sub(10,20));
   System.out.println(sub(10,20));
 }
 }
